58fa2e968d84f8f803e923606bbe98d05374ebb5
jabozaroid <araghibdoust@hotmail.com>
2025-02-03 15:35:29 +0330

    add_script

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+// with the help of nodemon we understand that the server is on development mode
+// if code will change, we need to command node index to see the changes,
+// but i wrote a script in package.json, so for do that just command "npm run dev" to server shows changes
+
 const express = require("express");
 var http = require("http");
 const cors = require("cors");
